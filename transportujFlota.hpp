2218db7427d449357e95034d6e110364c99b2ff9
Karol <bukowski.kar@gmail.com>
2024-11-05 17:16:23 +0100

    Zaliczenie v1

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,26 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0) {
+        return 0;
+    }
+
+    Stocznia     stocznia{};
+    unsigned int przetransportowano = 0;
+    unsigned int liczbaZaglowcow    = 0;
+
+    while (przetransportowano < towar) {
+        Statek* statek = stocznia();
+        if (statek) {
+            przetransportowano += statek->transportuj();
+
+            if (dynamic_cast< Zaglowiec* >(statek)) {
+                liczbaZaglowcow++;
+            }
+
+            delete statek;
+        }
+    }
+
+    return liczbaZaglowcow;
 }
